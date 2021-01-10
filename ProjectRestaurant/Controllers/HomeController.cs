0b7059695ef124483d90a987adb012def82ed194
Pinapala39 <37371360+Pinapala39@users.noreply.github.com>
2021-01-11 00:59:43 +0530

    Add files via upload

---
+++ ProjectRestaurant/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using APITemplate.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProjectRestaurant.Helper;
+using ProjectRestaurant.Models;
+
+namespace ProjectRestaurant.Controllers
+{
+    public class HomeController : Controller
+    {
+        DishesAPI _api = new DishesAPI();
+        public async Task<IActionResult> Index()
+        {
+            List<Dishes> dishes = new List<Dishes>();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage httpResponseMessage = await client.GetAsync("api/Dishes");
+            if(httpResponseMessage.IsSuccessStatusCode)
+            {
+                var result = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                dishes = JsonConvert.DeserializeObject<List<Dishes>>(result);
+            }
+            return View(dishes);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProjectRestaurant/Helper/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ProjectRestaurant.Helper
+{
+    public class DishesAPI
+    {
+        public HttpClient Initial()
+        {
+            var Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:57570/");
+            return Client;
+        }
+    }
+}
